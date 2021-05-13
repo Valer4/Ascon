6d4371a0e31e08d021ef619c3f0672e439286fbe
Valer4xD <valer4n1@yandex.ru>
2021-05-13 23:42:49 +0400

    DI в менеджеры

--- BusinessLogicLayer/Services/UnitsOfWork/Classes/AbstractUnitOfWorkService.cs
+++ BusinessLogicLayer/Services/UnitsOfWork/Classes/AbstractUnitOfWorkService.cs
@@ -7,7 +7,12 @@ namespace BusinessLogicLayer.Services.UnitsOfWork.Classes
     public abstract class AbstractUnitOfWorkService<TModel, TInterfaceUnitOfWorkManager>
         where TInterfaceUnitOfWorkManager : IAbstractUnitOfWorkManager<TModel>
     {
+        TInterfaceUnitOfWorkManager _unitOfWorkManager;
+
+        public AbstractUnitOfWorkService(TInterfaceUnitOfWorkManager repositoryManager) =>
+            _unitOfWorkManager = repositoryManager;
+
         [PrincipalPermission(SecurityAction.Demand, Role = AppRoles.Admin)]
-        public TModel Get() => Configurator._Container.Resolve<TInterfaceUnitOfWorkManager>().Get();
+        public TModel Get() => _unitOfWorkManager.Get();
     }
 }
